ec2d2408625aa6915ef10bbe446dbfbda7a1387e
Kumar Satyam <ksatyam.satyam@gmail.com>
2023-06-14 18:06:08 +0530

    Few string function exaples"

---
+++ StringFunctionExample.py
@@ -0,0 +1,29 @@
+fruit = 'Mango'
+print("fruit =",fruit)
+
+#Get the length of the string
+print("Lenght of fruit is ", len(fruit))
+
+print ('\n')
+
+# # Substring/Slicing/Part of string
+print ("Print the character present at 0th index.")
+print(fruit[0])
+
+print ("Print the character present from 0th till 3rd index", fruit[0:4])
+
+# 
+print ('\n')
+print ("fruit[:3] >", fruit[:3])
+print ("fruit[0:3] >", fruit[0:3])
+# 
+print ('\n')
+print ("fruit[0:] >", fruit[0:])
+print ("fruit[0:len(fruit)] >", fruit[0:len(fruit)])
+# 
+print ('\n')
+print ("fruit[:-1] >", fruit[:-1])
+print ("fruit[:len(fruit)-1] >", fruit[:len(fruit)-1])
+# 
+print ('\n')
+print ("fruit[1:3] >", fruit[1:3])
