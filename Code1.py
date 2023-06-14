8e861c3ec73f6d5cdd60c2e711dbb57e90347534
Kumar Satyam <ksatyam.satyam@gmail.com>
2023-06-14 13:01:58 +0530

    Basic operator use

---
+++ Code1.py
@@ -0,0 +1,9 @@
+a = 15
+b = 5
+
+print ("The value of" , a ,"+" , b ,"=" , a + b)
+print ("The value of" , a ,"-" , b ,"=" , a - b)
+print ("The value of" , a ,"*" , b ,"=" , a * b)
+print ("The value of" , a ,"/" , b ,"=" , a / b)
+print ("The value of" , a ,"//" , b ,"=" , a // b)
+print ("The value of" , a ,"%" , b ,"=" , a % b)
