e98cd54c60c2fe0cbe02b8c33bb6ad0ab471b807
Kumar Satyam <ksatyam.satyam@gmail.com>
2023-06-14 18:35:51 +0530

    String with menthods

---
+++ StringMethodExample.py
@@ -0,0 +1,32 @@
+str = "!!!Satyam !!!!! Satyam !!!!!"
+
+print ("Sample string >>", str)
+print ("Length of string >>", len(str))
+print (str.upper())
+print (str.lower())
+print (str.rstrip('!'))
+print (str.lstrip('!'))
+
+str1="welcome to india"
+str2="welcome to India"
+print (str1.capitalize())
+print (str2.capitalize())
+print("\n")
+print(str1.center(50))
+print('\n')
+print("print(str.count('Satyam')) >",str.count('Satyam'))
+print('\n')
+print("print(str.split(" ")) > ",str.split(" "))
+
+# isalnum()
+# endswith()
+# find()
+# index()
+# islower()
+# isprintable()
+    # \n is not a printable character
+# isspace()
+# istitle()
+# startswith()
+# swapcase()
+# title()    
