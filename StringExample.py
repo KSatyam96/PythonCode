680600ec6abdfb9ea81a3622fb54c522232e49e9
Kumar Satyam <ksatyam.satyam@gmail.com>
2023-06-14 17:45:16 +0530

    String Example

---
+++ StringExample.py
@@ -0,0 +1,24 @@
+name = "Satyam"
+
+name1 = 'satyam'
+
+text = ''' Hi , hello how are 
+
+you'''
+
+print(name)
+print ('\n')
+print(name1)
+print ('\n')
+print(text)
+print ('\n')
+print (name[0])
+print (name[1])
+print (name[2])
+print (name[3])
+print ('\n')
+
+
+# Print all characters of a string using for loop
+for character in name:
+    print (character)
